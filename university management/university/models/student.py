10a6478b874dfcbfb56d3c1f75eabf9e00f58290
anupamasakkuru781-del <anupamasakkuru781@gmail.com>
2026-03-16 06:04:57 +0530

    Add files via upload
    university management

---
+++ university management/university/models/student.py
@@ -0,0 +1,8 @@
+# class students details
+class students:
+    def __init__(self,ID,Name,Branch,Phone_number,Email):
+        self.ID = ID
+        self.Name = Name
+        self.Branch = Branch
+        self.Phone_number = Phone_number
+        self.Email = Email
